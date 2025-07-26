0048039f4c08d2973ed05a0eb0f1258030ffca24
mike <babatunde50099@gmail.com>
2025-07-26 13:38:33 +0100

    updated main jsx and create some folders too

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
+import Home from './components/Home/Home.jsx'
+import Clubs from './components/Clubs/Clubs.jsx'
+import Favorites from './components/Favorites/Favorites.jsx'
+import { Leagues } from './components/Leagues/Leagues.jsx'
+import LiveScores from './components/LiveScores/LiveScores.jsx'
+import Players from './components/Players/Players.jsx'
+import TransferNews from './components/TransferNews/TransferNews.jsx'
+import ClubDetails from './components/ClubDetails/ClubDetails.jsx'
+import PlayerDetails from './components/PlayerDetails/PlayerDetails.jsx'
+
+const router = createBrowserRouter([
+  {path: '/', element: <Home/>},
+  {path: '/clubs', element: <Clubs/>},
+  {path: '/favorites', element: <Favorites/>},
+  {path: '/leagues', element: <Leagues/>},
+  {path: '/livescores', element: <LiveScores/>},
+  {path: '/players', element: <Players/>},
+  {path: '/transfernews', element: <TransferNews/>},
+  {path: '/clubs/:id', element: <ClubDetails/>},
+  {path: '/players/:id', element: <PlayerDetails/>}
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
