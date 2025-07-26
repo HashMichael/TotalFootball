a05281f596628b0b6973555875ffd659d54b089e
NijiWritesCode <user.email>
2025-07-26 14:01:58 -0700

    Added A Navbar And An Header Section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './Home.css'
+import Navbar from '../Sub-Components/Navbar/Navbar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+
+        <section className='bg_section'>
+          <h2>
+            Champions League Final : Arsenal vs. Barcelona
+          </h2>
+          <p>
+            The highly anticipated final is set to Sunday, June 10th. Will Aresnal Finally Win It's First Ever Champions League Or Is It Another Next Season?
+          </p>
+
+          <button>
+            Read More
+          </button>
+        </section>
+
+        <section className='news_section'>
+
+        </section>
+      </main>
+    </>
   )
 }
 
--- src/components/Sub-Components/Navbar/Navbar.jsx
+++ src/components/Sub-Components/Navbar/Navbar.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { Moon, Search } from 'lucide-react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import dp from '../../../assets/dp.jpeg'
+import logo from '../../../assets/logo_light.png'
+import { Link } from 'react-router-dom'
 
-export const Navbar = () => {
+const Navbar = () => {
+
+  const [userPic, setUserPic] = useState(dp);
+
   return (
-    <div>Navbar</div>
+    <div className='nav_cont'>
+      <header>
+        <nav>
+         <img src={logo} alt="logo" width={30} />
+          <h3>
+             Total Football
+          </h3>
+          <ul>
+            <li><Link className='link'>Home</Link></li>
+            <li><Link className='link'>Leagues</Link></li>
+            <li><Link className='link'>Clubs</Link></li>
+            <li><Link className='link'>Transfer News</Link></li>
+            <li><Link className='link'>Live Scores</Link></li>
+            <li><input type="text" placeholder='Search'/> <Search color='#333' size={20}/></li>
+            <li><div className="dark_mode"><Moon color='#ddd'/></div></li>
+            <li><img src={userPic} alt="dp" width={35} height={35}/></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   )
 }
+
+export default Navbar
